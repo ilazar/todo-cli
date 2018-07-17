8120a30247754541163b3810d4c34c5aebc2975e
Ioan Lazar <ilazar@8x8.com>
2018-07-17 12:40:05 +0300

    04-item-cli

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 require("regenerator-runtime/runtime");
 require('./re-intro');
 
-import Item from './Item';
 import { ItemStore } from './ItemStore';
+import itemCli from './itemCli';
 
-const itemStore = new ItemStore();
-
-try {
-  const item = itemStore.insert(new Item('js', false));
-  console.log(item);
-  itemStore.insert(new Item('node', true));
-  itemStore.update({ id: 1 }, { isActive: false });
-  itemStore.remove({ text: 'js' });
-  console.log(itemStore.find({}));
-  console.log(itemStore.count({}));
-  itemStore.insert(new Item());
-} catch(error) {
-  console.log(error.issues);
-}
-
+itemCli(new ItemStore());
